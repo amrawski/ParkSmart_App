c60bf6e66ef4a7cca4ffed6548b3eecb417dadff
amrawski <adammrawski@gmail.com>
2020-01-23 12:01:10 -0500

    resource fix

---
+++ app/src/androidTest/java/Park/Smart/LotNumber1.java
@@ -0,0 +1,15 @@
+package Park.Smart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LotNumber1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lot_number1);
+
+    }
+}
