134520034c66cee7f86e9a789c5bb1ad9a22672e
amrawski <adammrawski@gmail.com>
2020-01-22 23:08:16 -0500

    Initial Commit

---
+++ app/src/main/java/Park/Smart/MainActivity.java
@@ -0,0 +1,30 @@
+package Park.Smart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button lotOne;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lotOne = (Button) findViewById(R.id.pecLot);
+        lotOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openlotOne();
+            }
+        });
+    }
+
+    public void openlotOne() {
+        Intent intent = new Intent(this, LotNumber1.class);
+        startActivity(intent);
+    }
+}
