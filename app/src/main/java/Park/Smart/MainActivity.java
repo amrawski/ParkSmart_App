dc6188848177c6bc591e62c54dd139ee1462a329
amrawski <adammrawski@gmail.com>
2020-01-23 12:35:41 -0500

    Mainactivity Fix

--- app/src/main/java/Park/Smart/MainActivity.java
+++ app/src/main/java/Park/Smart/MainActivity.java
@@ -7,15 +7,14 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static Park.Smart.R.id.pecLot;
-
 public class MainActivity extends AppCompatActivity {
-    private Button lotOne = findViewById(pecLot);
+    private Button lotOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lotOne = findViewById(R.id.pecLot);
         lotOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
