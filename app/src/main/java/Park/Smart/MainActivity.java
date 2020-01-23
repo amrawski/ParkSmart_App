5634cb1babdbce93afa7470e8c8a81c943c45b1b
amrawski <adammrawski@gmail.com>
2020-01-23 11:39:03 -0500

    resource fix

--- app/src/main/java/Park/Smart/MainActivity.java
+++ app/src/main/java/Park/Smart/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lotOne = (Button) findViewById(R.id.pecLot);
+        lotOne = findViewById(R.id.pecLot);
         lotOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
