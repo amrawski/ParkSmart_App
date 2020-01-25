b78c643bf3718190d9e0f89d549a6cc0749bf9bc
amrawski <adammrawski@gmail.com>
2020-01-25 13:21:28 -0500

    Added author and description to main activity

--- app/src/main/java/Park/Smart/MainActivity.java
+++ app/src/main/java/Park/Smart/MainActivity.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+ *  App: Park Smart
+ *  Author: Adam Rawski
+ *  Date created: 11/09/19
+ *  Version 1.0
+ *  Last revision: 01/25/20
+ *
+ *  Application to view current parking space availability and percent full
+ *
+ ******************************************************************************/
+
 package Park.Smart;
 
 import android.content.Intent;
@@ -9,7 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button lotOne;
-
+//Set button to wait for a user to press a button to select a specific lot
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +33,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //When user presses the button to go to a specific lot navigate to that page
     public void openlotOne() {
         Intent intent = new Intent(this, LotNumber1.class);
         startActivity(intent);
     }
 }
+/*<!--       _
+        .__(.)< (MEOW)
+        \___)
+        ~~~~~~~~~~~~~~~~~~-->*/
