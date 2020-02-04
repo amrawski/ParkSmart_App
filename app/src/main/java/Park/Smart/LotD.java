2af667031d2b35b172e9a0ff1abf5e42692eb24b
amrawski <adammrawski@gmail.com>
2020-02-04 14:56:08 -0500

    renaming Java docs and other docs to lot name and added new functions for changing xml

--- app/src/main/java/Park/Smart/LotD.java
+++ app/src/main/java/Park/Smart/LotD.java
@@ -9,7 +9,12 @@ public class LotNumber1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lot_number1);
+        setContentView(R.layout.activity_lot_d);
+
+        //Set an id to the layout
+//         ConstraintLayout currentLayout = findViewById(R.id.textView57);
+
+  //      currentLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
 
     }
 }
