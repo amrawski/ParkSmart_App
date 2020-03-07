a18d593399ee02975c00b9c8a5bd035bb29d0ae6
Bryan Brauchler <bbrauchl@umich.edu>
2020-03-07 04:41:34 -0500

    Adding example for server access in java

--- app/src/main/java/com/example/parksmartapipull/ParkSmartServer.kt
+++ app/src/main/java/com/example/parksmartapipull/ParkSmartServer.kt
@@ -1,6 +1,7 @@
 package com.example.parksmartapipull
 
 import android.app.Activity
+import android.content.Context
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
@@ -10,24 +11,32 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
-object ParkSmartServer : Activity() {
-    @JvmStatic val base_URL: String = "http://lamp.engin.umd.umich.edu/~bbrauchl"
-    @JvmStatic val pull_URL_ext: String = "/api/pull.php"
-    @JvmStatic val queue : RequestQueue = Volley.newRequestQueue(this.applicationContext)
+class ParkSmartServer(ctx: Context) {
+    companion object {
+        @JvmStatic
+        val base_URL: String = "http://lamp.engin.umd.umich.edu/~bbrauchl"
+
+        @JvmStatic
+        val pull_URL_ext: String = "/api/pull.php"
+    }
+    private var queue: RequestQueue = Volley.newRequestQueue(ctx)
 
     @JvmOverloads
-    @JvmStatic fun pull(lot: String? = "Lot_D", cb: Response.Listener<JSONObject>,
+    fun pull(lot: String? = "Lot_D", cb: Response.Listener<JSONObject>,
                         ecb: Response.ErrorListener? = null) {
+
         //create an object to hold all the data from our request
         val requestJSON = JSONObject()
 
         //if the lot was specified, assign its value
         if (lot != null) requestJSON.put("Lot", lot)
 
+
+
         val request = JsonObjectRequest(Request.Method.POST, base_URL + pull_URL_ext, requestJSON, cb,
-            Response.ErrorListener { it: VolleyError ->
+            Response.ErrorListener { error: VolleyError ->
                 Log.e("ParkSmartServer", "Error! The pull request failed!")
-                if (ecb != null) ecb.onErrorResponse(it)
+                if (ecb != null) ecb.onErrorResponse(error)
         })
         queue.add(request)
     }
